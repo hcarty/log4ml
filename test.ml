c042087b2ac6f5a9bf7e815a210091e2350fb12f
Hezekiah M. Carty <hez@0ok.org>
2012-03-13 21:36:22 -0400

    Add a simple test

---
+++ test.ml
@@ -0,0 +1,11 @@
+open Batteries
+module E = Log.Easy
+
+let () =
+  let oc = IO.output_string () in
+  E.init oc `error;
+  E.set_prefix (fun _ -> "PREFIX: ");
+  E.log `error "test";
+  assert (IO.close_out oc = "PREFIX: test\n");
+  print_endline "ok"
+
